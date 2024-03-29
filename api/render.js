9c1d97333361a6cc77c163c94bfc02ed3886df21
sit-in-a-row <yeonsukjung1212@gmail.com>
2024-03-29 20:43:11 +0900

    to_vercel

---
+++ api/render.js
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default function (req, res) {
+    const userAgent = req.headers['user-agent'].toLowerCase();
+    let filePath = join(__dirname, '..', 'index.html');
+
+    if (/mobile|android|iphone|ipad|phone/i.test(userAgent)) {
+        filePath = join(__dirname, '..', 'mobile.html');
+    }
+
+    const fileContents = readFileSync(filePath, 'utf8');
+    res.send(fileContents);
+}
