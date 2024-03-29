0c1db954e82eba812f46b59022895d84e320e4db
sit-in-a-row <yeonsukjung1212@gmail.com>
2024-03-29 19:58:15 +0900

    to_vercel

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/articles', express.static(path.join(__dirname, 'articles')));
